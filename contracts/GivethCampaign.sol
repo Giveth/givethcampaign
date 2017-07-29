e1b0cd49fcf8c3bb072bd94c1efd267872234681
Jordi Baylina <jordi@baylina.cat>
2017-07-29 14:03:10 +0200

    Deps and escape hatch in campaign contract

--- contracts/GivethCampaign.sol
+++ contracts/GivethCampaign.sol
@@ -192,4 +192,27 @@ contract GivethCampaign is TokenController, Owned {
         vaultAddress = _newVaultAddress;
     }
 
+    //////////
+    // Safety Methods
+    //////////
+
+    /// @notice This method can be used by the controller to extract mistakenly
+    ///  sent tokens to this contract.
+    /// @param _token The address of the token contract that you want to recover
+    ///  set to 0 in case you want to extract ether.
+    function claimTokens(address _token) public onlyOwner {
+        if (tokenContract.controller() == address(this)) {
+            tokenContract.claimTokens(_token);
+        }
+        if (_token == 0x0) {
+            owner.transfer(this.balance);
+            return;
+        }
+
+        ERC20Token token = ERC20Token(_token);
+        uint256 balance = token.balanceOf(this);
+        token.transfer(owner, balance);
+        ClaimedTokens(_token, owner, balance);
+    }
+
 }
